21ae2ad2178244b3b3b04cdb0565da7ebc18ad1c
Enzo Deher <enzo.deher@etu.iut-tlse3.fr>
2025-09-15 16:46:56 +0200

    Enzo : Ajout de commentaires détaillant les problèmes présents dans le code

--- src/12_RefactoringGolf/exo1/kata.ts
+++ src/12_RefactoringGolf/exo1/kata.ts
@@ -8,6 +8,7 @@ export class Game {
   public Play(symbol: string, x: number, y: number): void {
     //if first move
     if (this._lastSymbol == ' ') {
+      //Problème : Commentaire incohérent avec le code ("if player is X" alors que vérification sur O)
       //if player is X
       if (symbol == 'O') {
         throw new Error('Invalid first player');
@@ -27,6 +28,7 @@ export class Game {
     this._toto.AddTileAt(symbol, x, y);
   }
 
+  //Problème : Ne vérifie que les lignes, pas les colonnes ni les diagonales
   public Winner(): string {
     //if the positions in first row are taken
     if (
@@ -58,12 +60,14 @@ export class Game {
       }
     }
 
+    //Problème : Incohérences de commentaires ("2nd row" alors que c’est la 3ème ligne)
     //if the positions in 2nd row are taken
     if (
       this._toto.TileAt(2, 0)!.Symbol != ' ' &&
       this._toto.TileAt(2, 1)!.Symbol != ' ' &&
       this._toto.TileAt(2, 2)!.Symbol != ' '
     ) {
+      //Problème : Incohérences de commentaires ("middle row" alors que c’est la 3ème ligne)
       //if middle row is full with same symbol
       if (
         this._toto.TileAt(2, 0)!.Symbol == this._toto.TileAt(2, 1)!.Symbol &&
@@ -99,6 +103,7 @@ class Board {
     return this._plays.find((t: Tile) => t.X == x && t.Y == y)!;
   }
 
+  //Problème : Code mort (tile jamais utilisé)
   public AddTileAt(symbol: string, x: number, y: number): void {
     //@ts-ignore
     const tile: Tile = { X: x, Y: y, Symbol: symbol };
